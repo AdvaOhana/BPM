aa80a7bd1136997bd634723c89856f19fffad98a
advaohana <adva1230@gmail.com>
2025-02-23 02:13:13 +0200

    Create UpdateMeasures function created function that can update users measures according to his id

--- Middleware/measures_Mid.js
+++ Middleware/measures_Mid.js
@@ -1,6 +1,6 @@
 async function AddMeasures(req, res,next) {
-    let user_id    = req.body.user_id;
-    let date= new Date().toISOString().split("T")[0];
+    let user_id    = parseInt(req.body.user_id);
+    let date=  new Date().toISOString().split("T")[0];
     let sys_high= Number(req.body.sys_high);
     let dia_low= Number(req.body.dia_low);
     let pulse= Number(req.body.pulse);
@@ -41,4 +41,34 @@ async function GetMeasures(req,res,next){
     }
     next();
 }
-module.exports={AddMeasures,GetMeasures};
+async function UpdateMeasures(req,res,next){
+    let idx             = req.body.idx;
+    let date= new Date().toISOString().split("T")[0];
+    let sys_high= Number(req.body.sys_high);
+    let dia_low= Number(req.body.dia_low);
+    let pulse= Number(req.body.pulse);
+
+    if (idx === undefined)throw new Error('Id is not valid, please check again.');
+    if (sys_high === undefined)throw new Error('Must enter a systolic value.');
+    if (dia_low === undefined)throw new Error('Must enter a diastolic value.');
+    if (pulse === undefined)throw new Error('Must enter a pulse value.');
+
+    let Query = `UPDATE measures SET `;
+    Query += ` date = '${date}' , `;
+    Query += ` sys_high = '${sys_high}' , `;
+    Query += ` dia_low = '${dia_low}' , `;
+    Query += ` pulse = '${pulse}' `;
+    Query += ` WHERE id = ${idx} `;
+
+    const promisePool = db_pool.promise();
+    let rows=[];
+    try {
+        [rows] = await promisePool.query(Query);
+        req.success=true;
+    } catch (err) {
+        req.success=false;
+        console.log(err);
+    }
+    next();
+}
+module.exports={AddMeasures,GetMeasures,UpdateMeasures};
