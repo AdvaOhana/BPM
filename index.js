7deb639dfd4036158ab5b4261339152cfc2b65f3
advaohana <adva1230@gmail.com>
2025-02-27 21:38:40 +0200

    Created swagger docs

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
 const port = 6487;
 
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const app = express();
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+const swaggerAutogen = require("swagger-autogen")();
+const swaggerUi = require("swagger-ui-express");
 
-let db_M = require('./database');
-global.db_pool = db_M.pool;
+const swaggerOutputFile = "./swagger-output.json";
+const routes = ["./Routers/*.js"];
 
-app.use(express.static(path.join(__dirname, "View")));
-app.get("/", (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, "View/home.html"));
-})
-app.get("/patients", (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, "View/allusers.html"));
-})
-app.get("/measure", (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, "View/addMeasures.html"));
-})
+const doc = {
+    info: {
+        title: "My API",
+        description: "BPM",
+    },
+    host: `localhost:${port}`,
+};
 
-const measures_R=require('./Routers/measures_R');
-app.use('/',measures_R);
+swaggerAutogen(swaggerOutputFile, routes, doc).then(() => {
+    const swaggerDocument = require(swaggerOutputFile);
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, { explorer: true }));
 
-const users_R=require('./Routers/users_R');
-app.use('/',users_R);
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.use(express.json());
+    app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(port, () => {
-    console.log(`Now listening on port http://localhost:${port}`);
-})
+    let db_M = require("./database");
+    global.db_pool = db_M.pool;
+
+    app.use(express.static(path.join(__dirname, "View")));
+    app.get("/", (req, res) => {
+        res.status(200).sendFile(path.join(__dirname, "View/home.html"));
+    });
+    app.get("/patients", (req, res) => {
+        res.status(200).sendFile(path.join(__dirname, "View/allusers.html"));
+    });
+    app.get("/measure", (req, res) => {
+        res.status(200).sendFile(path.join(__dirname, "View/addMeasures.html"));
+    });
+
+    const measures_R = require("./Routers/measures_R");
+    app.use("/", measures_R);
+
+    const users_R = require("./Routers/users_R");
+    app.use("/", users_R);
+
+    app.listen(port, () => {
+        console.log(`Now listening on port http://localhost:${port}`);
+    });
+});
