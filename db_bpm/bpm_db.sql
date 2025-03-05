291b1b1650483cfebc905f09171549bb7e0908ce
advaohana <adva1230@gmail.com>
2025-03-05 21:13:02 +0200

    Sql DB uploding the database and the data in the db

---
+++ db_bpm/bpm_db.sql
@@ -0,0 +1,19 @@
+﻿create database bpm_DB;
+use bpm_DB;
+CREATE TABLE IF NOT EXISTS `users` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`full_name` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `measures` (
+	`id` int AUTO_INCREMENT NOT NULL UNIQUE,
+	`user_id` int NOT NULL,
+	`date` date NOT NULL,
+	`sys_high` int NOT NULL,
+	`dia_low` int NOT NULL,
+	`pulse` int NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `measures` ADD CONSTRAINT `measures_fk1` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
---
+++ db_bpm/bpm_measures.sql
@@ -0,0 +1,58 @@
+﻿-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bpm_db
+-- ------------------------------------------------------
+-- Server version	9.0.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `measures`
+--
+
+DROP TABLE IF EXISTS `measures`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `measures` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `date` date NOT NULL,
+  `sys_high` int NOT NULL,
+  `dia_low` int NOT NULL,
+  `pulse` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `measures_fk1` (`user_id`),
+  CONSTRAINT `measures_fk1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `measures`
+--
+
+LOCK TABLES `measures` WRITE;
+/*!40000 ALTER TABLE `measures` DISABLE KEYS */;
+INSERT INTO `measures` VALUES (1,1,'2025-02-22',120,80,101),(2,1,'2025-02-25',150,100,89),(3,2,'2025-02-25',150,100,89),(5,2,'2025-03-05',230,50,79),(6,2,'2025-03-04',180,20,90),(7,3,'2025-03-05',190,50,45),(8,3,'2025-03-05',233,89,60),(9,4,'2025-03-05',121,87,79),(10,4,'2025-03-05',130,90,75),(11,5,'2025-03-05',220,140,99),(12,5,'2025-03-05',200,160,80),(13,3,'2025-03-05',300,200,100);
+/*!40000 ALTER TABLE `measures` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-03-05 20:58:25
